92de239596c7a0ce3f9bdc192cdc05138635594f
Zac Crawford <zwcrawford@gmail.com>
2019-04-19 00:13:49 -0500

    restarting machine

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -95,19 +95,62 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(student);
-        }
+        public async Task<IActionResult> Create(
+			[Bind("EnrollmentDate,FirstMidName,LastName")] Student student)
+		{
+			/*
+			This code adds the Student entity created by the ASP.NET Core MVC model binder to the Students entity
+			set and then saves the changes to the database. (Model binder refers to the ASP.NET Core MVC
+			functionality that makes it easier for you to work with data submitted by a form; a model binder
+			converts posted form values to CLR types and passes them to the action method in parameters. In this
+			case, the model binder instantiates a Student entity for you using property values from the Form
+			collection.)
 
-        // GET: Students/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+			You removed ID from the Bind attribute because ID is the primary key value which SQL Server will set
+			automatically when the row is inserted. Input from the user doesn't set the ID value.
+
+			Other than the Bind attribute, the try-catch block is the only change you've made to the scaffolded
+			code. If an exception that derives from DbUpdateException is caught while the changes are being saved,
+			a generic error message is displayed. DbUpdateException exceptions are sometimes caused by something
+			external to the application rather than a programming error, so the user is advised to try again.
+			Although not implemented in this sample, a production quality application would log the exception. For
+			more information, see the Log for insight section in Monitoring and Telemetry (Building Real-World
+			Cloud Apps with Azure).
+
+			The ValidateAntiForgeryToken attribute helps prevent cross-site request forgery (CSRF) attacks. The
+			token is automatically injected into the view by the FormTagHelper and is included when the form is
+			submitted by the user. The token is validated by the ValidateAntiForgeryToken attribute. For more
+			information about CSRF, see Anti-Request Forgery.
+			*/
+			try
+			{
+				/* 
+				This is server-side validation that you get by default; in a later tutorial you'll see how to add
+				attributes that will generate code for client-side validation also. The following code: 
+
+				[if (ModelState.IsValid)]
+
+				shows the model validation check in the Create method.
+				*/
+				if (ModelState.IsValid)
+				{
+					_context.Add(student);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index));
+				}
+			}
+			catch (DbUpdateException /* ex */)
+			{
+				//Log the error (uncomment ex variable name and write a log.
+				ModelState.AddModelError("", "Unable to save changes. " +
+					"Try again, and if the problem persists " +
+					"see your system administrator.");
+			}
+			return View(student);
+		}
+
+		// GET: Students/Edit/5
+		public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
